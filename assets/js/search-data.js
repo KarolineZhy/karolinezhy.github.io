e8a7e4c1f212723f8648162f4cd5502403678684
KarolineZhy <50633669+KarolineZhy@users.noreply.github.com>
2025-01-22 21:38:10 +0000

    Deploying to gh-pages from @ KarolineZhy/karolinezhy.github.io@7e7808d2df216215a97b71aaa2497bf70cd12e68 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,8 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+    },{id: "news-i-start-my-ph-d-at-technical-unviersity-munich-in-camp-chair",
+          title: 'I start my Ph.D at Technical Unviersity Munich in CAMP chair!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
