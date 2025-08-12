762bea517950a9d31be7f6ce82ecb25b3e56310e
KarolineZhy <50633669+KarolineZhy@users.noreply.github.com>
2025-08-12 11:46:04 +0000

    Deploying to gh-pages from @ KarolineZhy/karolinezhy.github.io@9dd54900a75213aed2bf9af458e92ae9f08c83f6 🚀

--- _news/2-start.md
+++
@@ -1,8 +0,0 @@
----
-layout: post
-date: 2025-06-17
-inline: true
-related_posts: false
----
-
-One paper was accepted to MICCAI! 
---
+++ assets/js/search-data.js
@@ -0,0 +1,112 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-publications",
+          title: "publications",
+          description: "You can also find my articles on my Google Scholar.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Materials for courses you taught. Replace this text with your description.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
+        },{id: "news-i-start-my-ph-d-at-technical-unviersity-munich-in-camp-chair",
+          title: 'I start my Ph.D at Technical Unviersity Munich in CAMP chair!',
+          description: "",
+          section: "News",},{id: "news-one-paper-was-accepted-to-miccai",
+          title: 'One paper was accepted to MICCAI!',
+          description: "",
+          section: "News",},{id: "projects-ultrasound-video-anomaly-detection",
+          title: 'Ultrasound Video Anomaly Detection',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%79%75%65.%7A%68%6F%75@%74%75%6D.%64%65", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/KarolineZhy", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/yue-zhou-173b2a173", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=c_nVxMcAAAAJ", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
